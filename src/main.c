201ceecdc9b95fbf5a3275f03a2ee9c09edfd7e3
kevinator47 <kmvega05@gmail.com>
2025-06-07 22:35:19 -0400

    fix: fixed typetable bug

--- src/main.c
+++ src/main.c
@@ -14,7 +14,8 @@ extern ASTNode* root_node;
 
 int main(int argc, char **argv) {
     //  1. Inicializacion tabla de simbolos
-    TypeTable* type_table = create_type_table();
+    extern TypeTable* type_table;
+    type_table = create_type_table();
 
     // 2. Creacion tipos builtin
     TypeDescriptor *number_type = create_builtin_type(HULK_Type_Number, "number");
@@ -53,7 +54,6 @@ int main(int argc, char **argv) {
     }
 
     // 4. Parsear la entrada
-
     int parse_result = yyparse();
     if (parse_result != 0 || root_node == NULL) {
         fprintf(stderr, "Error de parsing.\n");
@@ -62,7 +62,6 @@ int main(int argc, char **argv) {
     }
     if (yyin != stdin) fclose(yyin);
 
-    print_ast_node(root_node, 0); // Imprimir el AST para depuración
     if (root_node == NULL) {
     fprintf(stderr, "El AST está vacío. No se generará código LLVM.\n");
     return 1;
@@ -76,27 +75,27 @@ int main(int argc, char **argv) {
     print_ast_node(root_node, 0); // Imprimir el AST para depuración
 
     // 6. Generación de código LLVM
-    // const char* module_name = "hulk_module";
-    // LLVMCodeGenerator* generator = create_llvm_code_generator("hulk_module");
-    // LLVMModuleRef module = generate_code(root_node, generator);
+    const char* module_name = "hulk_module";
+    LLVMCodeGenerator* generator = create_llvm_code_generator("hulk_module");
+    LLVMModuleRef module = generate_code(root_node, generator);
 
-    // // 7. Imprimir a archivo
-    // if (module) {
-    //     char* output_filename = "output.ll";
-    //     char* error_message = NULL;
+    // 7. Imprimir a archivo
+    if (module) {
+        char* output_filename = "output.ll";
+        char* error_message = NULL;
        
-    //     if (LLVMPrintModuleToFile(module, output_filename, &error_message) != 0) {
-    //         fprintf(stderr, "Error al escribir el modulo LLVM: %s\n", error_message);
-    //         LLVMDisposeMessage(error_message);
-    //     } else {
-    //         fprintf(stderr, "Archivo '%s' generado exitosamente.\n", output_filename);
-    //     }
-    // } else {
-    //     fprintf(stderr, "La generacion de codigo LLVM fallo.\n");
-    //     destroy_llvm_code_generator(generator);
-    //     return 1;
-    // }
-    // destroy_llvm_code_generator(generator);
+        if (LLVMPrintModuleToFile(module, output_filename, &error_message) != 0) {
+            fprintf(stderr, "Error al escribir el modulo LLVM: %s\n", error_message);
+            LLVMDisposeMessage(error_message);
+        } else {
+            fprintf(stderr, "Archivo '%s' generado exitosamente.\n", output_filename);
+        }
+    } else {
+        fprintf(stderr, "La generacion de codigo LLVM fallo.\n");
+        destroy_llvm_code_generator(generator);
+        return 1;
+    }
+    destroy_llvm_code_generator(generator);
 
     // 8.Limpieza final
     free_type_table(type_table);
